80bf3e1e6f71fc3d51cd6aafbcb663cdaa42b91d
star <ebyeol628@gmail.com>
2024-04-22 13:11:45 +0900

    add FrontController

---
+++ src/com/angel/comm/FrontController.java
@@ -0,0 +1,24 @@
+package com.angel.comm;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class FrontController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doReq(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doReq(request, response);
+    }
+
+    private void doReq(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException{
+
+    }
+}
